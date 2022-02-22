b3998b62bf57f08d725da7f5eaa9a0b26d0c1757
csko19113 <94610537+csko19113@users.noreply.github.com>
2022-02-23 00:03:46 +0900

    道の作成

--- CaveMiner/Assets/Scripts/Main/Board/CreateDangeon.cs
+++ CaveMiner/Assets/Scripts/Main/Board/CreateDangeon.cs
@@ -29,9 +29,49 @@ public class CreateDangeon : MonoBehaviour
             }
         }
     }
-
-    private void CreateRoad(int roomStartX, int roomStartY, int GrobalRoadPointX, int GrobalRoadPointY)
+    private void CreateRoad(int roadStartX, int roadStartY, int GrobalRoadPointX, int GrobalRoadPointY)
     {
+        int RandomJudge = Random.Range(0, 2);
+        if (RandomJudge == 0)
+        {
+            while (roadStartX != GrobalRoadPointX)
+            {
+                //xが同じになるまで移動、現在地をfloorにを繰り返す(道にする)
+                roadStartX = CompareRoadPoint(roadStartX, GrobalRoadPointX);
+                boardData.Board[roadStartX, roadStartY] = 1;
+            }
 
+            while (roadStartY != GrobalRoadPointY)
+            {
+                //yが同じになるまで移動、現在地をfloorにを繰り返す(道にする)
+                roadStartY = CompareRoadPoint(roadStartY, GrobalRoadPointY);
+                boardData.Board[roadStartX, roadStartY] = 1;
+            }
+        }
+        else//y軸方向から移動する
+        {
+            while (roadStartY != GrobalRoadPointY)
+            {
+                roadStartY = CompareRoadPoint(roadStartY, GrobalRoadPointY);
+                boardData.Board[roadStartX, roadStartY] = 1;
+            }
+            while (roadStartX != GrobalRoadPointX)
+            {
+                roadStartX = CompareRoadPoint(roadStartX, GrobalRoadPointX);
+                boardData.Board[roadStartX, roadStartY] = 1;
+            }
+        }
+    }
+    private int CompareRoadPoint(int roadStartPoint, int GrobalRoadPoint)
+    {
+        if (roadStartPoint < GrobalRoadPoint)
+        {
+            roadStartPoint++;
+        }
+        else
+        {
+            roadStartPoint--;
+        }
+        return roadStartPoint;
     }
 }
