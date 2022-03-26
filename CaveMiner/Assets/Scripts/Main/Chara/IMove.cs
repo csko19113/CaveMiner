4d253439e447fe74b0507b2945c9bc4c748c4da4
csko19113 <94610537+csko19113@users.noreply.github.com>
2022-03-26 14:39:33 +0900

    キャラの移動クラスを実装

--- CaveMiner/Assets/Scripts/Main/Chara/IMove.cs
+++ CaveMiner/Assets/Scripts/Main/Chara/IMove.cs
@@ -5,8 +5,8 @@ namespace Cave.Main.Chara
 {
     public interface IMove
     {
-        void AttemptMove<T>(int horizontal, int vertical);
+        void AttemptMove<T>(int horizontal, int vertical) where T : Component;
         void MoveCheck(int xDir, int yDir, out RaycastHit2D hit);
-        IEnumerator MoveObject(Vector3 startPosition, Vector3 endPosition);
+        void MoveObject(Vector3 startPosition, Vector3 endPosition);
     }
 }
