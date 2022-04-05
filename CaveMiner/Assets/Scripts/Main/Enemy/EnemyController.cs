bf72373ac3cdcbb0055ac714ba1b55710888a7c5
csko19113 <94610537+csko19113@users.noreply.github.com>
2022-04-06 00:01:43 +0900

    エネミーの操作クラスの作成

---
+++ CaveMiner/Assets/Scripts/Main/Enemy/EnemyController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Cave.Main.Enemy
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] private EnemyMove enemyMove;
+        [SerializeField] private GameParam gameParam;
+    }
+}
